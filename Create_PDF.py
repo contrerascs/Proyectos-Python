c4396b1044a3e86dc1cb9f664b6e23a478e87187
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-06 23:57:21 -0600

    Crear PDF

--- Create_PDF.py
+++ Create_PDF.py
@@ -2,4 +2,26 @@ import pdfkit
 import jinja2
 from datetime import datetime
 
-mundo = "mundo"
+COY = 'Dan Campbell'
+CPOY = 'Kirk Cousins'
+DROY = 'Jared Verse'
+OROY = 'Jayden Daniels'
+DPOY = 'T.J. Watt'
+OPOY = 'Derrick Henry'
+MVP = 'Jared Goff'
+today_date = datetime.today().strftime("%d %b, %Y")
+
+context = {'COY': COY, 'CPOY': CPOY, 'DROY': DROY, 'OROY': OROY,
+           'DPOY': DPOY, 'OPOY': OPOY, 'MVP': MVP, 'today_date': today_date}
+
+template_loader = jinja2.FileSystemLoader('./')
+template_env = jinja2.Environment(loader=template_loader)
+
+html_template = 'Plantilla.html'
+template = template_env.get_template(html_template)
+output_text = template.render(context)
+
+config = pdfkit.configuration(wkhtmltopdf = '"D:\Program Files\wkhtmltopdf\lib\wkhtmltox.lib"')
+output_pdf = 'pdf_generado.pdf'
+
+pdfkit.from_string(output_text, output_pdf, configuration=config)
