ee781da483e80347ca5ed22637b921f641209067
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-09 23:48:56 -0600

    Automate docs files with python

--- Automate-Word/Automate-Word.py
+++ Automate-Word/Automate-Word.py
@@ -1,13 +1,27 @@
 from docxtpl import DocxTemplate
 from datetime import datetime
+import pandas as pd
 
 mi_numero = '498-113-3003'
 mi_correo = 'sam.contreras@gmail.com'
 mi_direccion = '#13 Cornelia Street, NY'
 date_today = datetime.today().strftime('%d %b, %Y')
 
-doc = DocxTemplate(r'Automate-Word/sp-plantilla-mi-info.docx')
-context = {'mi_nombre': 'Sam Contreras', 'mi_numero': mi_numero,
+doc = DocxTemplate(r'Automate-Word\sp-plantilla-rrhh-info.docx')
+my_context = {'mi_nombre': 'Sam Contreras', 'mi_numero': mi_numero,
            'mi_correo': mi_correo, 'mi_direccion': mi_direccion, 'fecha_hoy': date_today}
-doc.render(context)
-doc.save(r'Automate-Word/generated_doc.docx')
+
+df = pd.read_csv(r'Automate-Word/test_data.csv')
+for index, fila in df.iterrows():
+    context = {
+        'nombre_persona_rrhh' : fila['name'],
+        'correo': fila['email'],
+        'direccion': fila['address'],
+        'puesto_trabajo': fila['job'],
+        'nombre_empresa': fila['company'],
+        'numero_telefono': fila['phone_number'],
+    }
+
+    context.update(my_context)
+    doc.render(context)
+    doc.save(rf'Automate-Word/Genereted_Docs/generated_doc_{index}.docx')
