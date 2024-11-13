34c0ae7a3694d0ab65ba3af0537475c5984ec79e
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-12 22:32:00 -0600

    Agregamos parametros nuevos para la nube de palabras

--- WordCloud/WordCloud.py
+++ WordCloud/WordCloud.py
@@ -1,3 +1,5 @@
 import stylecloud
 stylecloud.gen_stylecloud(file_path=r'WordCloud/TaylorSwift.txt',
-                          icon_name='fa-graduation-cap')
+                          icon_name='fas fa-rocket',
+                          colors='purple',
+                          background_color='black')
