e8fcc2cef487fedbb17520a427572b81614bece3
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-21 00:23:02 -0600

    Automate txt

---
+++ Automate-TXT/Automate-TXT.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+#Crear lista de numeros de telefonos en un archivo TXT/CSV (10 digitos)
+numeros = np.random.randint(1000000000, 9999999999, size=100, dtype=np.int64)
+#Exportamos estos numeros a un archivo txt
+numeros.tofile(r'Automate-TXT/numero-telefonos.txt', sep='\n')
+#Exportamos estos numeros a un archivo csv
+numeros.tofile(r'Automate-TXT/numero-telefonos.csv', sep='\n')
