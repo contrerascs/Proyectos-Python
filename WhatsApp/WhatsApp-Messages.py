de58415d80331bc4aef77bfc72bb14de2e98d7fb
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-20 23:38:44 -0600

    Actualización

--- WhatsApp/WhatsApp-Messages.py
+++ WhatsApp/WhatsApp-Messages.py
@@ -1,5 +1,7 @@
 import pywhatkit
 
+#Ingresar numero manualmente para no mostrarlo en código
 numero_telefono = input("Ingresa numero de telefono: ")
 
+#Parametros "23","30" son la hora en la que se enviará el mensaje
 pywhatkit.sendwhatmsg("+52"+numero_telefono,"Test",23,30, 15, True, 2)
