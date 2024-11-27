c6acab5bc2adc26fcdaf851683f94e4310ea02ee
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-26 23:53:22 -0600

    Extraer tabla de PDF y exportar a CSV

--- Extract-Table-PDF/Extract-Table-PDF.py
+++ Extract-Table-PDF/Extract-Table-PDF.py
@@ -2,3 +2,6 @@ import camelot
 
 table = camelot.read_pdf(r'Extract-Table-PDF/foo.pdf', pages='1')
 print(table)
+
+table.export(r'Extract-Table-PDF/foo.csv',f='csv', compress=True)
+table[0].to_csv(r'Extract-Table-PDF/foo.csv')
