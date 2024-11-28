76ed2a2faee2dc3e48e37094f07d9a1c78ce5240
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-11-27 23:37:21 -0600

    Comentar código

--- Extract-Table-PDF/Extract-Table-PDF.py
+++ Extract-Table-PDF/Extract-Table-PDF.py
@@ -3,5 +3,6 @@ import camelot
 table = camelot.read_pdf(r'Extract-Table-PDF/foo.pdf', pages='1')
 print(table)
 
+#Exportar tabla a csv
 table.export(r'Extract-Table-PDF/foo.csv',f='csv', compress=True)
 table[0].to_csv(r'Extract-Table-PDF/foo.csv')
