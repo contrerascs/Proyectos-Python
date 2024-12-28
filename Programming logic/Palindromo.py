ed8550d6da2f88460071582687501ff3d238d1a7
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-12-27 23:26:18 -0600

    Programming logic

---
+++ Programming logic/Palindromo.py
@@ -0,0 +1,17 @@
+#Definimos la función para evaluar la palabra
+def palindromo(palabra):
+    #Eliminamos espacios y convertimos a minúsculas
+    sin_espacios = palabra.replace(" ", "").lower()
+    print(sin_espacios)
+    comparacion = str('')
+    for index,letra in enumerate(sin_espacios):
+        if letra == sin_espacios[-(index+1)]:
+            comparacion = comparacion+letra
+            if comparacion == sin_espacios:
+                print('La palabra es un Palindromo')
+        else:
+            print('La palabra no es un palindromo')
+            break
+
+palabra = str(input('Ingresa la primera palabra: '))
+palindromo(palabra)
