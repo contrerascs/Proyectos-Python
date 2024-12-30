88670e599c6bdd9f9832febcc5e8da67bb19ec5f
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-12-30 00:03:11 -0600

    Pascal triangle

--- Programming logic/Pascal.py
+++ Programming logic/Pascal.py
@@ -1,5 +1,16 @@
 def pascal(n):
-    if n == 0:
-        print('Ingresa un numero valido')
-    else:
-        pass
+    triangle = []
+    for i in range(n):
+        # Inicializar la nueva fila
+        row = [1] * (i + 1)
+         # Completar los valores intermedios de la fila
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+        # Agregar la fila al triángulo
+        triangle.append(row)
+
+    return triangle
+
+num = int(input('Ingresa el tamaño de la piramide'))
+print(pascal(num))
